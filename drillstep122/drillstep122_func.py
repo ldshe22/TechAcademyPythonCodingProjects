9dc36aa98114c614dd4abde7d58c5c16ed3b91b3
ldshe22 <45728433+ldshe22@users.noreply.github.com>
2019-04-16 12:45:46 -0600

    drill122
    122drill

---
+++ drillstep122/drillstep122_func.py
@@ -0,0 +1,48 @@
+
+
+from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
+#filename = filedialog.askopenfilename()
+#filename = filedialog.asksaveasfilename()
+#dirname = filedialog.askdirectory()
+
+import drillstep122
+import drillstep122_gui
+
+
+
+
+
+
+
+
+def OnButtonClick(self):
+        dirname = filedialog.askdirectory()
+       
+
+
+          
+        
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+
+#import drillstep121
+#import drillstep121_gui
+
+
+
+
+        
